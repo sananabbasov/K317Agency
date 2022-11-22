b8609a9fe42caf731bb35beec4740fb36c550440
sananabbasov <sanan_abbasov@list.ru>
2022-11-22 21:53:32 +0400

    Add project files.

---
+++ WebApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        public DbSet<Banner> Banners { get; set; }
+    }
+}
